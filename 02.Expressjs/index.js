8c35d1cfe06eda65b80e84a08db7bd67d5b5bb84
pathumlak <123624162+pathumlak@users.noreply.github.com>
2024-05-01 16:43:00 +0530

    update advance routing

--- 02.Expressjs/index.js
+++ 02.Expressjs/index.js
@@ -3,17 +3,6 @@ import express from 'express';
 
 const app = express()
 
-app.get('/student' , (req, res) => {
-    res.send("raed the students")
-})
-app.post('/student', (req, res) => {
-    res.send("create the student")
-})
-app.put('/student', (req,res) =>{
-    res.send("update the students")
-})
-app.delete('/student', (req, res)=>{
-    res.send("delete student")
-})
+
 
 app.listen(8000, ()=> console.log("server up"))

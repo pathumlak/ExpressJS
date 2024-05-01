a42cd2ad39d605d2660febf2e798e5cb1145414e
pathumlak <123624162+pathumlak@users.noreply.github.com>
2024-05-01 15:07:27 +0530

    Initial commit

--- 02.Expressjs/index.js
+++ 02.Expressjs/index.js
@@ -4,8 +4,10 @@ import express from 'express';
 const app = express()
 
 
-app.get('/',(req, res) => {
-    res.send("welcome to express js") // browse the localhost:8000 , you can read this
+//http://localhost:8000/product/iphone
+app.get('/product/iphone' ,(req, res) => {
+    res.send("this will work only provide product/iphone")
 })
+
 
 app.listen(8000, ()=> console.log("server up"))

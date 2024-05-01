e200d2f17cb69d3293f395ed5099aeb456af47d3
pathumlak <123624162+pathumlak@users.noreply.github.com>
2024-05-01 14:16:47 +0530

    initial

--- 01.Before Expressjs/index.js
+++ 01.Before Expressjs/index.js
@@ -1,3 +1,10 @@
-// before start the express js cd to current dir  using terminal
-//then hit => npm init -y to get package.json
-//then add the package.json "type":"module"
+import http from 'http';
+
+
+//Before start the express js we can use node js http modues to routing
+const server = http.createServer((req, res) =>{
+
+})
+server.listen(8000, ()=> console.log("server up"))
+
+// you have to open the browser and type localhost:8000

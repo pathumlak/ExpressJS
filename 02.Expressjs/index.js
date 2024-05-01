69a07091d757f544a307bb26c4b0abb6a1b4b6b6
pathumlak <123624162+pathumlak@users.noreply.github.com>
2024-05-01 18:16:42 +0530

    param

--- 02.Expressjs/index.js
+++ 02.Expressjs/index.js
@@ -1,9 +1,15 @@
 import express from 'express'
-
-
 const app =  express();
 
+app.param("id", (req, res ,next, id) => {
+    console.log(`id : ${id}`);
+    next();
 
+})
 
+app.get('/user/:id' ,(req, res) =>{
+    console.log("this is user id ");
+    res.send("response ok")
+} )
 
 app.listen(8000, ()=> console.log("server up"))

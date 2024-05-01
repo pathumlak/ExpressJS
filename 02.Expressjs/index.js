613222e0a64fac7555eddd44c5088a77c1b96fda
pathumlak <123624162+pathumlak@users.noreply.github.com>
2024-05-01 16:21:46 +0530

    initial

--- 02.Expressjs/index.js
+++ 02.Expressjs/index.js
@@ -3,11 +3,17 @@ import express from 'express';
 
 const app = express()
 
-
-//http://localhost:8000/product/iphone
-app.get('/product/iphone' ,(req, res) => {
-    res.send("this will work only provide product/iphone")
+app.get('/student' , (req, res) => {
+    res.send("raed the students")
 })
-
+app.post('/student', (req, res) => {
+    res.send("create the student")
+})
+app.put('/student', (req,res) =>{
+    res.send("update the students")
+})
+app.delete('/student', (req, res)=>{
+    res.send("delete student")
+})
 
 app.listen(8000, ()=> console.log("server up"))
